207da730db72b20e87a03fdb38a4e2364e1ebf13
Roman <59027103+tchamalat@users.noreply.github.com>
2025-12-29 14:52:53 +0100

    Oauth test

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,27 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const nextAuthOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl + "/dashboard";
+    },
+  },
+  trustHost: true,
+};
+
+const handler = NextAuth(nextAuthOptions);
+
+export { handler as GET, handler as POST };
