c41321eff0a5d75594e603782311eeea8377aae7
Roman <59027103+tchamalat@users.noreply.github.com>
2025-12-31 10:58:40 +0100

    sans ia

--- app/api/admin/groups/[groupId]/members/route.ts
+++ app/api/admin/groups/[groupId]/members/route.ts
@@ -39,7 +39,7 @@ export async function POST(
     },
     include: {
       group: {
-        select: { name: true, image: true },
+        select: { id: true, name: true },
       },
     },
   });
